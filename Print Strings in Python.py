4222202ead91a0472ee163d0ef46d01519c28267
Vinit Vijal <vineet.shanker@soesector17rohini.co.in>
2021-07-06 23:53:03 +0530

    edit in strings program

--- Print Strings in Python.py
+++ Print Strings in Python.py
@@ -1,4 +1,11 @@
 class prn():
     string = "Hi, Its my first OOP"
+    def sum(self,a,b,d = 0):
+        c = a + b + d
+        return c
+
+
 obj = prn()
 print(obj.string)
+abc = obj.sum(15, 16, 40)
+print("Value of C is ",abc)
